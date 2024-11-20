47f0a54066083266fb7ef3e981553f945817d64a
qvifle <qvifffle@gmail.com>
2024-11-20 18:42:12 +0500

    save

--- dist/utils/getFilePath.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const path_1 = require("path");
-const getFilePath = (fileName) => {
-    return (0, path_1.join)(__dirname, "../../", "input", fileName);
-};
-exports.default = getFilePath;
